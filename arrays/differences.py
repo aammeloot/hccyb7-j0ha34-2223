394a9f79499c04cf325a355a129c97715774a49b
Aurélien Ammeloot <aammeloot@gmail.com>
2022-11-22 14:13:11 +0000

    Added extra examples

---
+++ arrays/differences.py
@@ -0,0 +1,28 @@
+# Arrays
+# Arrays are collections of data of the same type
+# In a given order
+
+# Ex: in Python
+
+lottery_draw = [5, 12, 3, 25, 45, 18]
+student_list = ['Stewart', 'Margo', 'Kyle']
+
+# Lists
+# Lists are collections of data that can be mixed
+# In a given order
+
+various = ['Aurelien', 25, True, 45.6, 'Mike']
+
+# There's a subtle difference in the way they're
+# Represented in computer memory
+# In Python unless you use a specifc array() object,
+# We always use lists, albeit with a single type of
+# Data most of the time
+
+# Dictionaries
+# Dictionaries are collections of values of mixed types
+# attached to a number of keys. They are not ordered
+
+teacher = {'name': 'Aurelien','age': 40,'location': 'Edinburgh' }
+print(teacher['name'])
+
